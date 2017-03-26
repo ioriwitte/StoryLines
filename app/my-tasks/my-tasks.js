82d042f4883aa5b9a08c1b334a8eba586a64f35d
Martin <martin.funkquist@gmail.com>
2017-03-26 13:08:01 +0200

    Added example project to the controller.

--- app/my-tasks/my-tasks.js
+++ app/my-tasks/my-tasks.js
@@ -4,15 +4,21 @@
 angular.module('myApp.myTasks', [])
   .controller('MyTasksController', function ($scope) {
 
-    $scope.projectList = [
-      {
-        name : "Project1",
-        cards : [
-          {
-
-          }
-        ]
+    $scope.exampleProjects = [
+        {
+        "id": "4eea4ffc91e31d1746000046",
+        "name": "Example Board",
+        "desc": "This board is used in the API examples",
+        "lists": [{
+          "id": "4eea4ffc91e31d174600004a",
+          "name": "To Do Soon"
+        }, {
+          "id": "4eea4ffc91e31d174600004b",
+          "name": "Doing"
+        }, {
+          "id": "4eea4ffc91e31d174600004c",
+          "name": "Done"
+        }]
       }
     ]
-
   });
