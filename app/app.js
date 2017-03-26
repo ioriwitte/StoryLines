be2648e4f8b7c4c653eb1b45a7ea4b5f88547477
Martin <martin.funkquist@gmail.com>
2017-03-26 10:14:12 +0100

    Added sidebar as separate html file.

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@
 angular.module('myApp', [
   'ngRoute',
   'myApp.login',
+  'myApp.sidebar',
   'myApp.myTasks',
   'myApp.plannerHub',
   'myApp.projectPage',
---
+++ app/sidebar/sidebar.js
@@ -0,0 +1,7 @@
+/**
+ * Created by marti on 2017-03-26.
+ */
+angular.module('myApp.sidebar', [])
+  .controller('SidebarController', function ($scope) {
+
+});
